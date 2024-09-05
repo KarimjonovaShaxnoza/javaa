716f190dc3e59393f5c6a497958b49e840af1699
KarimjonovaShaxnoza <shaxnozakarimjonova100@gmail.com>
2024-09-05 16:23:36 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+let str = prompt("String tipiga doir ma'lumot kiriting:")
+
+let num = +prompt("Number tipiga doir ma'lumot kiriting:")
+
+
+let boolean = prompt("Boolean(true, false) tipiga doir ma'lumot kiriting:")
+
+confirm("siz kiritgan ma'lumot turlari to'g'ri ekanligiga ishonchingiz komilmi")
+
+
+alert(
+`Siz 1-kiritgan ma'lumotingiz: ${str} va ma'lumot turi ${typeof(str)},
+ Siz 2-kiritgan ma'lumotingiz: ${num} va ma'lumot turi ${typeof(num)},
+ Siz 3-kiritgan ma'lumotingiz: ${boolean} va ma'lumot turi ${typeof(boolean==boolean)},
+    `
+)
